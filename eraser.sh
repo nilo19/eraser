50cf15551eabe7088768a4b151bda9af55c97dce
Qi Ni <pomelonicky@gmail.com>
2022-05-05 15:49:13 +0800

    add volumes

--- eraser.sh
+++ eraser.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-crictl rmi ea335eea17ab9
+crictl rmi a7d07d59bf9e7
 
 if [ $? -ne 0 ]; then
-  echo "failed to remove image ea335eea17ab9"
+  echo "failed to remove image a7d07d59bf9e7"
 else
   echo "succeeded"
 fi
